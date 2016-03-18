ecc806f984e36681166c41bf13f309757ad1aa6f
Claudiu Popa <pcmanticore@gmail.com>
2016-03-18 11:20:58 +0000

    Correct the line annotation for the test

--- src/main/resources/docs/directory-tests/testapp/src/main/java/codacy/FindBugs.java
+++ src/main/resources/docs/directory-tests/testapp/src/main/java/codacy/FindBugs.java
@@ -1,6 +1,6 @@
 //#Patterns: FI_PUBLIC_SHOULD_BE_PROTECTED, FI_EMPTY
-//#Issue: {"severity": "Error", "line": 11, "patternId": "FI_PUBLIC_SHOULD_BE_PROTECTED"}
-//#Issue: {"severity": "Error", "line": 11, "patternId": "FI_EMPTY"}
+//#Issue: {"severity": "Error", "line": 12, "patternId": "FI_PUBLIC_SHOULD_BE_PROTECTED"}
+//#Issue: {"severity": "Error", "line": 12, "patternId": "FI_EMPTY"}
 package codacy;
 
 public class FindBugs {
