b3d4ac3a68989f2d99eec66af4e7857084513420
Claudiu Popa <pcmanticore@gmail.com>
2016-03-18 11:17:06 +0000

    Update the test for FindBugs.

---
+++ src/main/resources/docs/directory-tests/testapp/src/main/java/codacy/FindBugs.java
@@ -0,0 +1,14 @@
+//#Patterns: FI_PUBLIC_SHOULD_BE_PROTECTED, FI_EMPTY
+//#Issue: {"severity": "Error", "line": 11, "patternId": "FI_PUBLIC_SHOULD_BE_PROTECTED"}
+//#Issue: {"severity": "Error", "line": 11, "patternId": "FI_EMPTY"}
+package codacy;
+
+public class FindBugs {
+
+    public static void main(final String[] args) {
+    }
+
+    public void finalize() {
+    }
+                 
+}
